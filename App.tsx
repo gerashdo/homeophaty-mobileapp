4814b1db6998c51a93f40b27deef13b6f24509a6
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-18 15:04:53 -0600

    Add navigation base with NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola mundo</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>Hola mundo</Text>
+      </View>
+    </NavigationContainer>
   )
 }
 
