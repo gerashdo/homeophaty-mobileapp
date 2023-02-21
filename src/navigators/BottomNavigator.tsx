5c00d2b7140cf87ca7c0612657551b3c14f5b1bd
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-21 15:20:28 -0600

    Unify tab navigator

--- src/navigators/BottomNavigator.tsx
+++ src/navigators/BottomNavigator.tsx
@@ -1,14 +1,61 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useContext } from 'react';
 import { Platform } from 'react-native';
-import { BottomTabsAndroid } from './BottomTabsAndroid';
-import { BottomTabsIOS } from './BottomTabsIOS';
+import { TabBarIcon } from '../components/TabBarIcon';
+import { ThemeContext } from '../context/theme/ThemeContext';
+import { MedicinesListScreen } from '../screens/MedicinesListScreen';
+import { ProfileScreen } from '../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomNavigator = () => {
+  const { theme: { colors } } = useContext( ThemeContext )
+
   return (
-    Platform.OS === 'ios'
-        ? <BottomTabsIOS />
-        : <BottomTabsAndroid />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: colors.background
+      }}
+      screenOptions={{
+        headerStyle:{
+          backgroundColor: colors.background,
+          elevation: 0,
+          borderBottomWidth: 0,
+        },
+        tabBarActiveTintColor: colors.primary,
+        tabBarStyle: {
+          elevation: 0,
+          borderTopWidth: 0,
+          height: (Platform.OS === 'android') ? 70 : 80,
+          paddingBottom: (Platform.OS === 'android') ? 10 : 20,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle:{
+          fontWeight: '500',
+        },
+        tabBarInactiveTintColor: colors.text,
+      }}
+    >
+      <Tab.Screen 
+        name="MedicinesListScreen" 
+        component={ MedicinesListScreen }
+        options={{
+          tabBarLabel: "Medicamentos",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon color={ color } focused={ focused } name="medkit"/>
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="ProfileScreen" 
+        component={ ProfileScreen }
+        options={{
+          tabBarLabel: "Perfil",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon color={ color } focused={ focused } name="person-circle"/>
+          )
+        }}
+      />
+    </Tab.Navigator>
   );
 }
