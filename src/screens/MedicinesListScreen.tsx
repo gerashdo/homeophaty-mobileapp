fa90ad045785045ac810e457872201a993437d37
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-21 16:27:25 -0600

    Load medicines from backend

--- src/screens/MedicinesListScreen.tsx
+++ src/screens/MedicinesListScreen.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Text, View } from 'react-native'
+import { CustomActivityIndicator } from '../components/ActivityIndicator'
+import { MedicineContext } from '../context/medicine/MedicineContext'
+import { ScreenTemplate } from './ScreenTemplate'
 
 export const MedicinesListScreen = () => {
+
+  const { loadMedicines, isLoading, medicineState } = useContext( MedicineContext )
+  const { medicines } = medicineState
+
+  useEffect(() => {
+    loadMedicines()
+  }, [])
+
   return (
-    <View>
-        <Text>MedicinesListScreen</Text>
-    </View>
+    <ScreenTemplate>
+      {
+        isLoading
+          ? ( <CustomActivityIndicator /> )
+          : (
+            <View>
+                {
+                  medicines && (
+                    medicines.map( med => (
+                      <Text key={ med.id }>{ med.name } { med.ch }</Text>
+                    ))
+                  )
+                }
+                <Text>MedicinesListScreen</Text>
+            </View>
+          )
+      }
+    </ScreenTemplate>
   )
 }
