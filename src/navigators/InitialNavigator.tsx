6c3e32bb38a226b231f46988d0c2b45ab27d6a3d
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-20 21:11:45 -0600

    Redirect to main navigation if user authenticates

--- src/navigators/InitialNavigator.tsx
+++ src/navigators/InitialNavigator.tsx
@@ -1,20 +1,40 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { useContext } from 'react';
+import { AuthContext } from '../context/auth/AuthContext';
+import { AuthStatus } from '../context/auth/authReducer';
 import { LoginScreen } from '../screens/LoginScreen';
 import { BottomNavigator } from './BottomNavigator';
 
 const Stack = createStackNavigator();
 
 export const InitialNavigator = () => {
+
+    const { state } = useContext( AuthContext )
+    const { status } = state
+
     return (
         <Stack.Navigator>
-        <Stack.Screen 
-            name="LoginScreen" 
-            component={ LoginScreen }
-            options={{
-                headerShown: false,
-            }}
-        />
-        <Stack.Screen name="BottomNavigator" component={ BottomNavigator } />
+            {
+                status === AuthStatus.AUTHENTICATED
+                    ? (
+                        <Stack.Screen 
+                            name="BottomNavigator" 
+                            component={ BottomNavigator }
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                    )
+                    :(
+                        <Stack.Screen 
+                            name="LoginScreen" 
+                            component={ LoginScreen }
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                    )
+            }
         </Stack.Navigator>
     );
 }
