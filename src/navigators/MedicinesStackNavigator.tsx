30da27f2d42e23fc67d98f5bea6a193a635f1576
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-22 17:45:39 -0600

    Add medicine details screen

--- src/navigators/MedicinesStackNavigator.tsx
+++ src/navigators/MedicinesStackNavigator.tsx
@@ -4,15 +4,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NewMedicineScreen } from '../screens/NewMedicineScreen';
 import { MedicinesListScreen } from '../screens/MedicinesListScreen';
 import { ThemeContext } from '../context/theme/ThemeContext';
+import { MedicineScreen } from '../screens/MedicineScreen';
 
 export enum MedicinesRoutes {
     MEDICINES_LIST = "MedicinesListScreen",
-    NEW_MEDICINE = "NewMedicineScreen"
+    NEW_MEDICINE = "NewMedicineScreen",
+    MEDICINE_DETAILS = "MedicineScreen",
 }
 
 export type MedicinesRootStackParamList = {
     MedicinesListScreen: undefined,
     NewMedicineScreen: undefined,
+    MedicineScreen: undefined,
 }
 
 const Stack = createStackNavigator<MedicinesRootStackParamList>();
@@ -39,6 +42,7 @@ export const MedicinesStackNavigator = () => {
                 component={ MedicinesListScreen }
             />
             <Stack.Screen name={ MedicinesRoutes.NEW_MEDICINE } component={ NewMedicineScreen }/>
+            <Stack.Screen name={ MedicinesRoutes.MEDICINE_DETAILS } component={ MedicineScreen }/>
         </Stack.Navigator>
     )
 }
