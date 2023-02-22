5451aab971ffa304b2720ad839f6c5f8a5dcd579
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-21 19:21:20 -0600

    Implement search med action with debouncer

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { TextInput } from 'react-native'
+import { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { appStyles } from '../theme/appTheme'
 import { InputContainer } from './InputContainer'
-import { useState } from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { useDebouncedValue } from '../hooks/useDebounce';
 
 interface Props {
     onSearch: ( value: string ) => void;
@@ -14,6 +16,12 @@ export const SearchInput = ({ onSearch, textColor }:Props) => {
 
     const [ searchValue, setSearchValue ] = useState('')
 
+    const debouncedValue = useDebouncedValue( searchValue, 600 )
+
+    useEffect(() => {
+      onSearch( debouncedValue )
+    }, [ debouncedValue ])
+    
     return (
         <InputContainer
             style={{
